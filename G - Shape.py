f9d8af6ebd025ea0f98ada310c9332a9ae9da62d
Edward Stark <98830123+StarkEdward@users.noreply.github.com>
2022-05-13 22:50:09 +0530

    G shape pattern

---
+++ G - Shape.py
@@ -0,0 +1,11 @@
+#Date 13/05/2022 
+#Python Pattern Program that prints "G" letter
+
+for r in range(7):
+	for c in range(5):
+		if (c == 0 and (r!=0 and r!= 6)) or ((r == 0 or r == 6) and(c>0 and c<4)) or (c == 4 and (r> 0 and r != 2 and r != 3 and r!=6)) or (r==4 and c> 1):
+			print("*", end=" ")
+		else:
+			print(end="  ")
+	print()
+print()

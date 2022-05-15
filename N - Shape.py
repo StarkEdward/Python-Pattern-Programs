8703bd13572bc15072d87a569b458dff342c4a8f
StarkEdward <skamble454@gmail.com>
2022-05-15 21:57:44 +0530

    added more letter patterns

--- N - Shape.py
+++ N - Shape.py
@@ -1,2 +1,9 @@
 # Python Pattern Program Printing Stars in "N" Shape
 
+for r in range(6):
+    for c in range(6):
+        if (c == 0 or c == 5) or (r == c and (c > 0 and c < 5)):
+            print("*", end=" ")
+        else:
+            print(end="  ")
+    print()

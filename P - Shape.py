ba71762393c73b42d34039274c24382cb0825769
StarkEdward <skamble454@gmail.com>
2022-05-16 20:10:12 +0530

    added more letter patterns

---
+++ P - Shape.py
@@ -0,0 +1,9 @@
+# Python Pattern Program Printing Stars in "P" Shape
+
+for r in range(7):
+    for c in range(5):
+        if ((c ==0 and r > 0)) or ((r == 0 or r == 3) and (c>0 and c<4)) or ((r == 1 or r == 2) and c==4):
+            print("*", end=" ")
+        else:
+            print(end="  ")
+    print()

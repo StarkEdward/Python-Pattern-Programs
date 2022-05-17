4f85e71f39fbd9a97be74cc9e2a5d9983cff5cbb
StarkEdward <skamble454@gmail.com>
2022-05-17 19:49:15 +0530

    added more letter patterns

---
+++ R - Shape.py
@@ -0,0 +1,8 @@
+# Python Pattern Program Printing Stars in "R" Shape
+for r in range(7):
+	for c in range(5):
+		if c== 0 or ((r == 0 or r == 3) and(c<4)) or (c == 4 and (r>0 and r< 3)) or (r == c+2 and c>0):
+			print("*",end=" ")
+		else:
+			print(end="  ")
+	print()

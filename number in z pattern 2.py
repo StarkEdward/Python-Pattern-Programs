a177669f092bd758147a54371685cb388d4bcb3e
StarkEdward <skamble454@gmail.com>
2022-05-24 20:00:26 +0530

    number pattern z

--- number in z pattern 2.py
+++ number in z pattern 2.py
@@ -4,8 +4,8 @@ n = input("Enter an odd length numbers: ")
 length = len(n)
 for r in range(length):
     for c in range(length):
-        if (r == c) or (r+c == length -1):
-            print(n[r],end=" ")
+        if (r == c) or (r + c == length - 1 ):
+            print(n[c], end=" ")
         else:
             print(end="  ")
     print()

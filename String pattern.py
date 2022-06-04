3066e03662b127906c7726acc356d6e0e001b013
StarkEdward <skamble454@gmail.com>
2022-06-04 20:24:48 +0530

    string pattern

--- String pattern.py
+++ String pattern.py
@@ -1,6 +1,6 @@
 str1 = input("Enter String: ")
 length = len(str1)
-for i in range():
-    for j in range():
+for i in range(length+1):
+    for j in range(i):
         print(str1[j], end = " ")
     print()
